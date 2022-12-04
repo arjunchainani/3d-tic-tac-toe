217b7c51dac999ff9f3c1395826f7d63f9260d4e
ArjunChainani <arjun.k.chainani@gmail.com>
2022-12-03 19:05:43 -0500

    experimenting with different geometries

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 // Scene
 const scene = new THREE.Scene();
-scene.background = new THREE.Color(0xffffff);
+scene.background = new THREE.Color(0xff33ff);
 
 // Canvas
 const canvas = document.querySelector('canvas.webgl');
@@ -14,16 +14,22 @@ const sizes = {
 const loadingManager = new THREE.LoadingManager();
 const textureLoader = new THREE.TextureLoader(loadingManager);
 
-const colorTexture = textureLoader.load('/images/board1.png');
+const colorTexture = textureLoader.load('/images/minecraft.png');
 colorTexture.generateMinmaps = false;
 colorTexture.minFilter = THREE.NearestFilter;
 colorTexture.magFilter = THREE.NearestFilter;
 
 // Cube
-const geometry = new THREE.BoxGeometry(1, 1, 1);
-const material = new THREE.MeshBasicMaterial( { map: colorTexture } );
-const cube = new THREE.Mesh(geometry, material);
-scene.add(cube);
+const geometry = new THREE.BufferGeometry();
+const vertices = new Float32Array([
+    -10.0, -10.0, 0.0, 
+    10.0, -10.0, 0.0,
+    10.0, 10.0, 0.0,
+])
+
+const material = new THREE.MeshLambertMaterial( { color: 0xF78F13 } );
+const sphere = new THREE.Mesh(geometry, material);
+scene.add(sphere);
 
 // Camera
 const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 0.1, 100);
@@ -31,6 +37,20 @@ camera.position.x = 0;
 camera.position.y = 0;
 camera.position.z = 3;
 scene.add(camera);
+
+// Lighting
+const spotlight = new THREE.SpotLight(0xfcff33);
+spotlight.position.set(0, 130, 0);
+spotlight.target.position.set(400, -130, 0);
+scene.add(spotlight.target);
+scene.add(spotlight);
+
+// const ambient = new THREE.AmbientLight(0xffffff, 0.5);
+// ambient.add(ambient);
+
+// const light = new THREE.PointLight(0x03d3fc, 2.0, 0.5);
+// scene.add(light);
+
 
 // Render
 const renderer = new THREE.WebGLRenderer({
@@ -50,24 +70,13 @@ window.addEventListener('resize', function() {
     renderer.setSize(sizes.width, sizes.height);
 })
 
-// Mouse Dragging
-const controls = new DragControls(objects, camera, window);
-
-controls.addEventListener('dragstart', function(event) {
-    event.object.material.emissive.set(0xaaaaaa);
-})
-
-controls.addEventListener('dragend', function(event) {
-    event.object.material.emissive.set(0x000000);
-})
-
 // Animation
 function animate() {
     requestAnimationFrame(animate);
 
-    cube.rotation.x += 0.01;
-    cube.rotation.y += 0.01;
-    cube.rotation.z += 0.01;
+    sphere.rotation.x += 0.01;
+    sphere.rotation.y += 0.01;
+    sphere.rotation.z += 0.01;
 
     renderer.render(scene, camera);
 }
